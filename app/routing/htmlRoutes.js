c5c083fbc0f85b1a86a0c9934122463980377dec
Thomas Martin <tmartin.dev101010@gmail.com>
2018-11-27 21:29:09 -0700

    moving to desktop computer

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,7 @@
 var path = require("path");
 
 module.exports = function (app) {
-    
+
     // Serve the home page
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
@@ -10,5 +10,10 @@ module.exports = function (app) {
     // Serve up the survey page
     app.get('/survey', function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    // Serve the home page for anything else
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
